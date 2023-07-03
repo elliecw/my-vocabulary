820150986c9e02b600a5547ed4316230b740f83b
elliecw <hanchig@gmail.com>
2023-07-03 23:26:55 +0800

    add express-session and login feature via passport

--- app.js
+++ app.js
@@ -1,4 +1,7 @@
 const express = require('express')
+const session = require('express-session')
+const usePassport = require('./config/passport')
+const passport = require('passport')
 const exphbs = require('express-handlebars')
 const methodOverride = require('method-override')
 const bcrypt = require('bcryptjs')
@@ -12,11 +15,30 @@ const User = db.User
 
 app.engine('hbs', exphbs.engine({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
+
+app.use(session({
+  secret: 'ThisIsMySecret',
+  resave: false,
+  saveUninitialized: true
+}))
+
 app.use(express.urlencoded({ extended: true }))
 app.use(methodOverride('_method'))
 
+// 呼叫 Passport 函式並傳入 app，這條要寫在路由之前
+usePassport(app)
+
+// app.get('/', (req, res) => {
+//   res.render('index')
+// })
+
 app.get('/', (req, res) => {
-  res.render('index')
+  return Word.findAll({
+    raw: true,
+    nest: true
+  })
+    .then((words) => { return res.render('index', { words: words }) })
+    .catch((error) => { return res.status(422).json(error) })
 })
 
 app.post('/search', (req, res) => {
@@ -38,9 +60,10 @@ app.get('/users/login', (req, res) => {
   res.render('login')
 })
 
-app.post('/users/login', (req, res) => {
-  res.send('login')
-})
+app.post('/users/login', passport.authenticate('local', {
+  successRedirect: '/',
+  failureRedirect: '/users/login'
+}))
 
 app.get('/users/register', (req, res) => {
   res.render('register')
@@ -48,13 +71,34 @@ app.get('/users/register', (req, res) => {
 
 app.post('/users/register', (req, res) => {
   const { name, email, password, confirmPassword } = req.body
-  User.create({ name, email, password })
-    .then(user => res.redirect('/'))
+  User.findOne({ where: { email } }).then(user => {
+    if (user) {
+      console.log('User already exists')
+      return res.render('register', {
+        name,
+        email,
+        password,
+        confirmPassword
+      })
+    }
+    return bcrypt
+      .genSalt(10)
+      .then(salt => bcrypt.hash(password, salt))
+      .then(hash => User.create({
+        name,
+        email,
+        password: hash
+      }))
+      .then(() => res.redirect('/'))
+      .catch(err => console.log(err))
+  })
 })
 
 app.get('/users/logout', (req, res) => {
-  res.send('logout')
+  req.logout()
+  res.redirect('/users/login')
 })
+
 app.listen(PORT, () => {
   console.log(`App is running on http://localhost:${PORT}`)
 })
